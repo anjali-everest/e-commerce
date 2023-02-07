5a88bdf1d99b8c39723fb2e0e2139f7c095098fd
Gaddam Anjali <anjali.gaddam@everest.engineering>
2023-02-07 12:54:34 +0530

    Implement add item to cart API

--- routes/item_router.py
+++ routes/item_router.py
@@ -5,6 +5,12 @@ item_router = APIRouter(prefix="/item")
 
 item_service = ItemService()
 
+
 @item_router.get("/all")
 def get_items():
     return item_service.get_items()
+
+
+@item_router.post("/add_to_cart")
+def add_item_to_cart(item_id: int):
+    return item_service.add_item_to_cart(item_id)
--- services/item.py
+++ services/item.py
@@ -3,7 +3,25 @@ import json
 
 class ItemService:
     def get_items(self):
-        f = open('data.json')
+        f = open('data/data.json')
         data = json.load(f)
         f.close()
         return data
+
+    def add_item_to_cart(self, item_id: int):
+        f = open('data/data.json')
+        data = json.load(f)
+        f.close()
+        for item in data:
+            if item['id'] == item_id:
+                item['addedToCart'] = True
+
+        # Serializing json
+        json_object = json.dumps(data, indent=4)
+
+        # Writing back to file
+        with open("data/data.json", "w") as outfile:
+            outfile.write(json_object)
+            outfile.close()
+
+        return {"Added item to cart!"}
